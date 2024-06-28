e1e97276ff74ea3a92b86a55da22959c981bb45a
PersuesDale <67306565+PersuesDale@users.noreply.github.com>
2024-06-28 18:54:23 +0530

    CustomerRegister completed

--- client/src/Pages/customer/CustomerRegistration.js
+++ client/src/Pages/customer/CustomerRegistration.js
@@ -6,7 +6,8 @@ import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import './CustomerRegister.css';
 import { useNavigate } from "react-router-dom";
-import img from '../../images/image 72.png'
+import img from '../../images/image 75.png'
+import img2 from '../../images/Component 5.svg'
 
 function CustomerRegistration() {
 
@@ -16,6 +17,122 @@ function CustomerRegistration() {
   };
 
   return (
+
+    <div className="CustomerRegistration">
+
+    <div className="CustomerRegistration-contain">
+
+      <div className="mt-5">
+
+        <img src={img} alt=""/>
+
+      </div>
+
+      <div className="CustomerRegistration-input-div">
+
+        <h1 className="CustomerRegistration-input-div-h1">Customer Register</h1>
+        
+        <div className="CustomerRegistration-input-div-div">
+
+          <div className="CustomerRegistration-input-div-leftdiv">
+
+            <div>
+
+              <h1 className="CustomerRegistration-input-div-leftdiv-h1">Name</h1>
+              <input type="text" className="CustomerRegistration-input-div-leftdiv-inp" placeholder="Name"></input>
+
+            </div>
+
+            <div>
+
+              <h1 className="CustomerRegistration-input-div-leftdiv-h1">Gender</h1>
+              <div className="CustomerRegistration-input-radio">
+                <input type="Radio" className="CustomerRegistration-input-div-leftdiv-inp-radio" ></input>
+                <label className="mt-2">M</label>
+                <input type="Radio" className="CustomerRegistration-input-div-leftdiv-inp-radio" ></input>
+                <label className="mt-2">F</label>
+                <input type="Radio" className="CustomerRegistration-input-div-leftdiv-inp-radio" ></input>
+                <label className="mt-2">Other</label>
+              </div>
+            </div>
+
+            <div>
+              
+              <h1 className="CustomerRegistration-input-div-leftdiv-h1">Address</h1>
+              <input type="select" className="CustomerRegistration-input-div-leftdiv-inp" placeholder="Address"></input>
+
+            </div>
+
+            <div>
+
+              <h1 className="CustomerRegistration-input-div-leftdiv-h1">District</h1>
+              <select className="CustomerRegistration-input-div-leftdiv-inp">
+                <option>Select Your District</option>
+              </select>
+            </div>
+
+            <div>
+              
+              <h1 className="CustomerRegistration-input-div-leftdiv-h1">City</h1>
+              <input type="text" className="CustomerRegistration-input-div-leftdiv-inp" placeholder="City"></input>
+
+            </div>
+
+          </div>
+
+          <div className="CustomerRegistration-input-div-rightdiv">
+
+            <div>
+
+              <h1 className="CustomerRegistration-input-div-leftdiv-h1">Pincode</h1>
+              <input type="text" className="CustomerRegistration-input-div-leftdiv-inp" placeholder="Pincode"></input>
+
+            </div>
+
+            <div>
+
+              <h1 className="CustomerRegistration-input-div-leftdiv-h1">Contact Number</h1>
+              <input type="text" className="CustomerRegistration-input-div-leftdiv-inp" placeholder="Contact Number"></input>
+
+            </div>
+
+            <div>
+
+              <h1 className="CustomerRegistration-input-div-leftdiv-h1">Email ID</h1>
+              <input type="select" className="CustomerRegistration-input-div-leftdiv-inp" placeholder="Email Id"></input>
+
+            </div>
+
+            <div>
+
+              <h1 className="CustomerRegistration-input-div-leftdiv-h1">Password</h1>
+              <input type="text" className="CustomerRegistration-input-div-leftdiv-inp" placeholder="Password"></input>
+              <button className="CustomerRegistration-inputcontain-div-pwordbutton"><img src={img2} alt=""/></button>
+
+            </div>
+
+            <div>
+
+              <h1 className="CustomerRegistration-input-div-leftdiv-h1">Confirm Password </h1>
+              <input type="text" className="CustomerRegistration-input-div-leftdiv-inp" placeholder="Re-enter Password"></input>
+              <button className="CustomerRegistration-inputcontain-div-pwordbutton"><img src={img2} alt=""/></button>
+              
+            </div>
+
+          </div>
+          
+        </div>
+
+        <button className="CustomerRegistration-input-div-button">Register</button>
+        <p className="CustomerRegistration-input-div-p">Already have an account? <a href=" " alt="">Log in</a></p>
+        
+      </div>
+
+    </div>
+    
+
+  </div>
+
 
 
     // <div className="customer_register">
