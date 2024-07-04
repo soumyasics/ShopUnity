7965fefc4d9bb429efc76cf4c10dab6e816c6cdf
ajeena <ajeena.sics@gmail.com>
2024-07-04 11:43:42 +0530

    done

--- client/src/Pages/Admin/AdminLogin.js
+++ client/src/Pages/Admin/AdminLogin.js
@@ -5,8 +5,8 @@ import Col from "react-bootstrap/Col";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import { Link, useNavigate } from "react-router-dom";
-import "./Admin.css"
-import adminlog from '../../images/adminlog.png'
+import "./Admin.css";
+import adminlog from "../../images/adminlog.png";
 function AdminLogin() {
   const [data, setData] = useState({
     email: "",
@@ -23,8 +23,6 @@ function AdminLogin() {
 
   const Navigate = useNavigate();
 
-
-
   const formValidating = (fieldName, value) => {
     if (!value.trim()) {
       return `${fieldName} is required`;
@@ -33,7 +31,7 @@ function AdminLogin() {
     if (fieldName === "Email" && !value.endsWith("@gmail.com")) {
       return "Email must be a valid Gmail address";
     }
- 
+
     if (fieldName === "Password") {
       const passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[A-Z]).{6,}$/;
       if (!passwordRegex.test(value)) {
@@ -77,57 +75,67 @@ function AdminLogin() {
     <div>
       <Row>
         <Col className="col-7">
-          <img className='adminlog mt-5' src={adminlog} alt="img"></img>
+          <img className="adminlog mt-5" src={adminlog} alt="img"></img>
         </Col>
         <Col className="col-5">
           <div className="container">
-        <form  className="p-3">
-          <div className="mb-3 container">
-             <h1 className=" mt-5 container" id="signin">Admin Login</h1>
-             <label className="font" htmlFor="form-controler-email">
-               Email
-             </label>
-             <input
-              type="email"
-              className="form-control form-input"
-              id="textbox"
-              placeholder="Email"
-              required=""
-              onChange={handleInputChange}
-              name="email"
-              value={data.email}
-            />
-             {errors.email && <div className="text-danger">{errors.email}</div>}
-             <span className="glyphicon form-control-feedback" />
-           </div>
-           <div className="form-group has-feedback container" id="form-group">
-             <label className="font" htmlFor="form-controler-password">
-               Password
-             </label>
-             <input
-               type="password"
-               className="form-control form-input"
-               id="textbox"
-               placeholder="Password"
-               required=""
-               name="password"
-               onChange={handleInputChange}
-               value={data.password}
-             />
-             {errors.password && (
-               <div className="text-danger">{errors.password}</div>
-             )}
-             <span className="glyphicon form-control-feedback" />
-           </div>
-           </form>
-           <div className=" container mt-4">
-            
-            <button type="submit" className="btn" onClick={handleSubmit} id="text">
-               Login
-             </button>
-           </div>
-           
-         </div>
+            <form className="p-3">
+              <div className="mb-3 container">
+                <h1 className=" mt-5 container" id="signin">
+                  Admin Login
+                </h1>
+                <label className="font" htmlFor="form-controler-email">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control form-input"
+                  id="textbox"
+                  placeholder="Email"
+                  required=""
+                  onChange={handleInputChange}
+                  name="email"
+                  value={data.email}
+                />
+                {errors.email && (
+                  <div className="text-danger">{errors.email}</div>
+                )}
+                <span className="glyphicon form-control-feedback" />
+              </div>
+              <div
+                className="form-group has-feedback container"
+                id="form-group"
+              >
+                <label className="font" htmlFor="form-controler-password">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control form-input"
+                  id="textbox"
+                  placeholder="Password"
+                  required=""
+                  name="password"
+                  onChange={handleInputChange}
+                  value={data.password}
+                />
+                {errors.password && (
+                  <div className="text-danger">{errors.password}</div>
+                )}
+                <span className="glyphicon form-control-feedback" />
+              </div>
+            </form>
+            <div className=" container mt-4">
+              <button
+                type="submit"
+                className="btn"
+                onClick={handleSubmit}
+                id="text"
+              >
+                Login
+              </button>
+            </div>
+          </div>
         </Col>
       </Row>
     </div>
