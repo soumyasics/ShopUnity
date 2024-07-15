8b5c91f5e408b36942a3aefd5f7c63c48734f9fb
Ajeenasics <ajeena.sics@gmail.com>
2024-07-15 14:37:52 +0530

    done

--- client/src/APIS/axiosMultipartInstance.js
+++ client/src/APIS/axiosMultipartInstance.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosMultipartInstance = axios.create({
 
-   baseURL: "http://localhost:4028/shopunity_api",
+   // baseURL: "http://localhost:4028/shopunity_api",
   
-   // baseURL:"http://hybrid.srishticampus.in/shopunity_api/",
+   baseURL:"http://hybrid.srishticampus.in/shopunity_api/",
 
    headers: {
     "Content-Type": "multipart/form-data", 
